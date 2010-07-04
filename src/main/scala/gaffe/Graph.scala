50de1c8054f11707a2c867c93218af4903258f0e
Stu Hood <stuhood@gmail.com>
2010-07-04 02:13:05 -0400

    Add multiple paths atomically

--- src/main/scala/gaffe/Graph.scala
+++ src/main/scala/gaffe/Graph.scala
@@ -10,23 +10,29 @@ class Graph(generation: Long) {
     // a graph is an immutable set of edges and vertices
     private val ref: AtomicReference[GraphRef] = new AtomicReference(new GraphRef(0, HashMap.empty, HashSet.empty))
 
-    def add(path: Path): Unit = {
+    /**
+     * Atomically adds the given paths to this graph.
+     */
+    def add(paths: Path*): Unit = {
         while (true) {
             // atomically get a graph reference
             val graph = ref.get
 
-            // add the path to the graph
-            var vertices = canonicalize(path.source, graph.vertices)
+            // add each path to the graph
+            var vertices: (Vertex, HashMap[Value, Vertex]) = (null, graph.vertices)
             var edges = graph.edges
-            for (edge <- path.edges.iterator) {
-                val source = vertices._1
-                vertices = canonicalize(edge.dest, vertices._2)
-                // clone the outbound edge with the canonicalized destination
-                val e = new OutEdge
-                e.label = edge.label
-                e.dest = vertices._1
-                // and add to the set of edges
-                edges = edges + new Edge(source, e)
+            for (path <- paths) {
+                vertices = canonicalize(path.source, vertices._2)
+                for (edge <- path.edges.iterator) {
+                    val source = vertices._1
+                    vertices = canonicalize(edge.dest, vertices._2)
+                    // clone the outbound edge with the canonicalized destination
+                    val e = new OutEdge
+                    e.label = edge.label
+                    e.dest = vertices._1
+                    // and add to the set of edges
+                    edges = edges + new Edge(source, e)
+                }
             }
 
             // update the graph reference
