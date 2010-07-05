e40b0c369e51545565f3a97bd1e6c85001757e98
Stu Hood <stuhood@gmail.com>
2010-07-05 01:48:56 -0400

    Convert to sorted maps in preparation for introduction of queries

--- src/main/scala/gaffe/Graph.scala
+++ src/main/scala/gaffe/Graph.scala
@@ -1,13 +1,13 @@
 
 package gaffe
 
-import java.util.concurrent.atomic.AtomicReference
-import scala.collection.mutable.{HashMap, HashSet}
+import java.util.TreeMap
+
 import scala.collection.JavaConversions.asIterator
 
 class Graph(generation: Long) {
     var version: Long = 0
-    private val vertices: HashMap[Value,Adjacencies] = HashMap.empty
+    private val vertices: TreeMap[Value,Adjacencies] = new TreeMap
 
     /**
      * Assumes that the given odd numbered list of Values represents alternating Vertices and Edges, and adds is as a path in the graph.
@@ -28,30 +28,42 @@ class Graph(generation: Long) {
             val out = new Edge
             out.label = edge
             out.vertex = dest.vertex
-            source.outs + out
+            source.outs.put(out, 0)
             // inbound
             val in = new Edge
             in.label = edge
             in.vertex = source.vertex
-            dest.ins + in
+            dest.ins.put(in, 0)
 
             source = dest
         }
-        version = version + 1
+        version = 1 + version
+    }
+
+    /**
+     * Returns an iterator over paths matching the given odd numbered list of Values (representing alternating
+     * Vertices and Edges), where option None indicates 'any value'.
+     */
+    def get(values: Option[Value]*): Iterator[Seq[Value]] = {
+        throw new RuntimeException("Not implemented")
     }
 
     /**
      * Adds a value to the given vertices, and returns the canonical version of the vertex and its adjacencies.
      */
-    private def canonicalize(value: Value, vertices: HashMap[Value,Adjacencies]): Adjacencies = {
-        vertices.get(value).getOrElse({
-            // place it in the graph
-            val adjacencies = new Adjacencies(new Vertex, Set.empty, Set.empty)
-            adjacencies.vertex.name = value
-            adjacencies.vertex.gen = -1
-            adjacencies.vertex.block = -1
-            vertices.put(value, adjacencies)
-            adjacencies})
+    private def canonicalize(value: Value, vertices: TreeMap[Value,Adjacencies]): Adjacencies = {
+        vertices.get(value) match {
+            case null =>
+                // place it in the graph
+                val adjacencies = new Adjacencies(new Vertex, new TreeMap, new TreeMap)
+                adjacencies.vertex.name = value
+                adjacencies.vertex.gen = -1
+                adjacencies.vertex.block = -1
+                vertices.put(value, adjacencies)
+                adjacencies
+            case x =>
+                x
+        }
     }
 
     override def toString: String = {
@@ -59,6 +71,6 @@ class Graph(generation: Long) {
     }
 
     // adds adjacency lists to a Vertex
-    final class Adjacencies(val vertex: Vertex, val ins: Set[Edge], val outs: Set[Edge])
+    final class Adjacencies(val vertex: Vertex, val ins: TreeMap[Edge, Long], val outs: TreeMap[Edge, Long])
 }
 
--- src/test/scala/gaffe/GraphSpecs.scala
+++ src/test/scala/gaffe/GraphSpecs.scala
@@ -1,6 +1,8 @@
 
 package gaffe
 
+import scala.collection.mutable.WrappedArrayBuilder
+
 import java.nio.ByteBuffer
 
 import org.apache.avro.Schema
@@ -11,30 +13,35 @@ import org.scalatest.matchers.ShouldMatchers
 
 class GraphSpecs extends FlatSpec with ShouldMatchers
 {
+    def values(strings: String*) = for (string <- strings) yield {
+        val value = new Value
+        value.value = ByteBuffer.wrap(string.getBytes)
+        value
+    }
+
+    // implicitly convert values to optional values
+    implicit def vals2optvals(vals: Seq[Value]) = vals.map(Option(_))
+
     "A Graph" should "allow individual vertices to be added" in {
         val graph = new Graph(0)
         val oldversion = graph.version
-
-        val src = new Value
-        src.value = ByteBuffer.wrap("src".getBytes)
-
-        graph.add(src)
+        graph.add(values("src"): _*)
         graph.version should not equal (oldversion)
     }
 
     it should "allow paths to be added" in {
         val graph = new Graph(0)
         val oldversion = graph.version
-
-        val src = new Value
-        src.value = ByteBuffer.wrap("src".getBytes)
-        val edge = new Value
-        edge.value = ByteBuffer.wrap("edge".getBytes)
-        val dest = new Value
-        dest.value = ByteBuffer.wrap("dest".getBytes)
-
-        graph.add(src, edge, dest)
+        graph.add(values("src", "edge", "dest"): _*)
         graph.version should not equal (oldversion)
+    }
+
+    it should "allow exact matches for paths" in {
+        val graph = new Graph(0)
+        graph.add(values("src", "edge", "dest"): _*)
+        
+        val iter = graph.get(values("src", "edge", "dest"): _*)
+        iter.hasNext should be (true)
     }
 }
 
