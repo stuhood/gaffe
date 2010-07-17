b2740c030bd2217d75534c97e4addac4bd53599f
Stu Hood <stuhood@gmail.com>
2010-07-17 15:34:47 -0400

    Simplify and remove duplication in MemoryGenSpecs

--- src/test/scala/gaffe/MemoryGenSpecs.scala
+++ src/test/scala/gaffe/MemoryGenSpecs.scala
@@ -10,53 +10,36 @@ import org.scalatest.matchers.ShouldMatchers
 
 class MemoryGenSpecs extends FlatSpec with ShouldMatchers
 {
-    "A MemoryGen" should "allow individual vertices to be added" in {
+    def memgen(paths: List[Value]*): MemoryGen = {
         val graph = new MemoryGen(0)
-        val oldversion = graph.version
-        graph.add(values("src"))
-        graph.version should not equal (oldversion)
+        for (path <- paths) graph.add(path)
+        graph
     }
 
-    it should "allow paths to be added" in {
-        val graph = new MemoryGen(0)
-        val oldversion = graph.version
-        graph.add(values("src", "edge", "dest"))
-        graph.version should not equal (oldversion)
-    }
-
-    it should "allow exact matches for a vertex" in {
-        val graph = new MemoryGen(0)
-        graph.add(values("src", "edge", "dest"))
-        
+    "A MemoryGen" should "allow exact matches for a vertex" in {
+        val graph = memgen(values("src", "edge", "dest"))
         graph.get(Query(values("src"))) should be === (Stream(values("src")))
         graph.get(Query(values("dest"))) should be === (Stream(values("dest")))
     }
 
     it should "allow exact matches for a path" in {
-        val graph = new MemoryGen(0)
-        graph.add(values("src", "edge", "dest"))
-        
+        val graph = memgen(values("src", "edge", "dest"))
         graph.get(Query(values("src", "edge", "dest"))) should be === (Stream(values("src", "edge", "dest")))
     }
 
     it should "not match a missing vertex" in {
-        val graph = new MemoryGen(0)
-        graph.add(values("src", "edge", "dest"))
-        
+        val graph = memgen(values("src", "edge", "dest"))
         graph.get(Query(values("not there"))) should be === (Stream.empty)
     }
 
     it should "fail to match a malformed path" in {
-        val graph = new MemoryGen(0)
-        graph.add(values("src", "edge", "dest"))
-        
+        val graph = memgen(values("src", "edge", "dest"))
         evaluating {graph.get(Query(values("src", "edge")))} should produce [IllegalArgumentException]
     }
 
     it should "support ordered iteration" in {
-        val graph = new MemoryGen(0)
-        graph.add(values("apple", "tastes", "sweet"))
-        graph.add(values("banana", "tastes", "edgy"))
+        val graph = memgen(values("apple", "tastes", "sweet"),
+            values("banana", "tastes", "edgy"))
         
         for (adjacencies <- graph.iterator) yield {
             adjacencies.name
